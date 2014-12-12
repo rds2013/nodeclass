5e24e1efe25a5ceeb12807bfd8c3f8d644de9769
“Robert <davids@qawired.com>
2014-12-12 12:37:36 -0800

    First express app

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-var http = require('http');
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res){
+    res.send("Welcome to Hollywood!");
+
+});
+app.listen(8080);
